3236bf000b4b3f4f1f3966de14151196c4767676
Tiago Alves <tsasa@iscte.iul.pt>
2021-11-15 23:32:20 +0000

    --commit test

--- main.py
+++ main.py
@@ -1,20 +1,93 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from httplib2 import Http
+import json
 import time
 
 hostName = "localhost"
 serverPort = 8080
 
+Clients='{' \
+        '' \ 
+        '}'
+
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s teste 123</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        #================== Recebemos o primeiro GET
+        request_client = self.requestline
+        ip = self.client_address[0]
+        print("request: " + request_client)
+        print(ip)
+
+        # ====================Estamos a trabalhar no GET para o DOCKER
+        h = Http()
+        with open("Server.txt", "w+") as servers:
+            for server in servers:
+                uri="http://"
+                uri+=server
+
+                header = h.request(uri,"HEAD")
+                conten_type = header[0]['content-type'].split(';')[0]
+
+
+                response,content = h.request(uri)
+                print("content:",content)
+                print("response:",response)
+
+                break
+            if conten_type == "text/html":
+                pass
+            if conten_type == "text/json":
+                pass
+
+            #=============Response para o cliente
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+            self.wfile.write(bytes(content))
+            #self.send_response(response)
+
+
+        with open("myfile.json","w") as jsonFile:
+            json.dump(self.Dados_to_Json("pedido1",ip,request_client),jsonFile)
+            jsonFile.close()
+  
+
+        '''
+        #Escreve no ficheiro o ip do cliente que fez o pedido
+        #Se já existir esse ip lá, ignora
+        existe = False
+        try:
+            f = open("Clientes.txt", "r")
+            for line in f:
+                if line == str(ip):
+                    existe=True
+                    break
+                f.close()
+                if existe == False:
+                    f = open("Clientes.txt", "r")
+                    conteudo = f.readlines()
+                    conteudo.append(ip)
+                    f.close()
+                    f = open("Clientes.txt", "w")
+                    f.writelines(conteudo)
+                    f.close()
+        except:
+            f = open("Clientes.txt", "w")
+            f.close()
+    '''
+
+
+
+
+
+'''
+    def Dados_to_Json(self,nome,ip,pedido):
+        s = {}
+        return s
+
+'''
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
@@ -26,4 +99,4 @@ if __name__ == "__main__":
         pass
 
     webServer.server_close()
-    print("Server stopped.")
+    print("Server stopped.") 
